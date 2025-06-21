f4d75890269e5e01a905d936e43e340b9a6a2662
Andre Marques <andrem166@gmail.com>
2025-06-21 18:50:00 -0300

    formulario e input criados

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaltInput } from './components/DefaltInput';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -17,6 +18,23 @@ export function App() {
       </Container>
       <Container>
         <CountDown />
+      </Container>
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <DefaltInput id='task' type='text' labelText='Task' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
---
+++ src/components/DefaltInput/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.css';
+type DefaltInputProps = {
+  id: string;
+  labelText?: string;
+} & React.ComponentProps<'input'>;
+
+export function DefaltInput({ id, type, labelText, ...rest }: DefaltInputProps) {
+  return (
+    <>
+      <label htmlFor={id}>{labelText}</label>
+      <input className={styles.input} id={id} type={type} {...rest} />
+    </>
+  );
+}
