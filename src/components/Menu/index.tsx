a3327433626a9246bf163ad089144bda568ca037
Andre Marques <andrem166@gmail.com>
2025-06-22 21:40:13 -0300

    Botão de alterar tema funcionando

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,60 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
-import { MenuLink } from './MenuLink';
+import React, { useEffect, useState } from 'react';
 
-const links = [
-  { href: '#', icon: <HouseIcon /> },
-  { href: '#', icon: <HistoryIcon /> },
-  { href: '#', icon: <SettingsIcon /> },
-  { href: '#', icon: <SunIcon /> },
-];
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function toogleTheme(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    setTheme(prevTheme => (prevTheme === 'dark' ? 'light' : 'dark'));
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    console.log(theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      {links.map(({ href, icon }) => (
-        <MenuLink href={href}>{icon}</MenuLink>
-      ))}
+      <a
+        className={styles.menuLink}
+        href='#'
+        title='Ir para a Home'
+        aria-label='Ir para a Home'
+      >
+        <HouseIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        title='Ver Histórico'
+        aria-label='Ver Histórico'
+      >
+        <HistoryIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        title='Configurações'
+        aria-label='Configurações'
+      >
+        <SettingsIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        title='Alterar o Tema'
+        aria-label='Alterar o Tema'
+        onClick={toogleTheme}
+      >
+        <SunIcon />
+      </a>
     </nav>
   );
 }
